1b60224905f0ade5ad0cbf4810b728aab7ded706
sangpk <dinhsangcnc@gmail.com>
2022-08-01 11:46:20 +0700

    file

--- file.js
+++ file.js
@@ -1,9 +1,48 @@
+const imgPosition=document.querySelectorAll(".aspect-ratio-169 img");
+const imgContainer=document.querySelectorAll('.aspect-ratio-169');
+const dotItem= document.querySelectorAll(".dot");
+let imgNumber= imgPosition.length;
+let index=0;
+imgPosition.forEach(function (image,index){
+    image.style.left= index*100 + "%";
+    dotItem[index].addEventListener("click", function () {
+        slider(index);
+    })
+})
+function imgSlide(){
+    index++;
+    if(index>=imgNumber){index=0}
+    slider(index);
+}
+function slider(index){
+    let imgContainers=document.getElementsByClassName("aspect-ratio-169")[0];
+    let imgs = imgContainers.querySelectorAll("img");
+    for (let i = 0; i < imgs.length; i++) {
+        if (i === index) {
+            let a = "0%";
+            imgs[i].style.left = a;
+        }
+        else  {
+            let a = "-" +(i%imgNumber*100)+ "%";
+            imgs[i].style.left = a;
+        }
+    }
+    // let imgContainer = imgContainers.querySelectorAll("img")[index];
+    // // let a = "-" +(index*100)+ "%";
+    // // console.log(a);
+    // let a = "0%";
+    // // console.log(index);
+    // imgContainer.style.left = a;
+    const dotActive=document.querySelector('.active');
+    dotActive.classList.remove("active")
+    dotItem[index].classList.add("active")
+}
+setInterval(imgSlide,3000)
+
+//////////////dang nhap///////////////////////////////
 var inputUsername=document.getElementById("username");
 var inputPassword=document.getElementById("password");
-
-
 var formLogin=document.getElementById("form-login");
-
 if(formLogin.attachEvent){
     formLogin.attachEvent('submit', onFormSubmit)
 }else {
